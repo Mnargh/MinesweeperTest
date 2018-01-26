83a274c23a327582d3e511d1d88da1ff0710510c
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-26 11:24:37 +0000

    Enzyme Jest functioning. Single test of div

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="foo">
+        <p className="bar">test!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
